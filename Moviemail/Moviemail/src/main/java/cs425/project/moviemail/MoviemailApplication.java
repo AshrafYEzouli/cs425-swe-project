f5072339a5726ca60372f332272bf9193d4df9c7
aezouli <aezouli@miu.edu>
2022-04-18 14:17:19 -0500

    init empty spring boot project

---
+++ Moviemail/Moviemail/src/main/java/cs425/project/moviemail/MoviemailApplication.java
@@ -0,0 +1,13 @@
+package cs425.project.moviemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviemailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviemailApplication.class, args);
+    }
+
+}
---
+++ Moviemail/Moviemail/src/test/java/cs425/project/moviemail/MoviemailApplicationTests.java
@@ -0,0 +1,13 @@
+package cs425.project.moviemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviemailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
