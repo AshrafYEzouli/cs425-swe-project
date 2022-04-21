87f22c15c2542e60ef9ea38d6ca89027bce1eb57
aezouli <aezouli@miu.edu>
2022-04-21 09:33:57 -0500

    bug fix

--- Moviemail/Moviemail/src/main/java/cs425/project/moviemail/config/MovieMailWebAppSecurityConfig.java
+++ Moviemail/Moviemail/src/main/java/cs425/project/moviemail/config/MovieMailWebAppSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -56,6 +57,12 @@ public class MovieMailWebAppSecurityConfig extends WebSecurityConfigurerAdapter 
                 .permitAll()
                 .and()
                 .exceptionHandling();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/admin/new");
+        web.ignoring().antMatchers("/customer/new");
     }
 }
 
